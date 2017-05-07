1dd5600e4adb1b7cc31cc67bf51239aa22279646
Mengxiong10 <15623530292@163.com>
2017-05-07 22:31:22 +0800

    demo init

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <div class="demo">
+      <span class="label">默认:</span>
+      <date-picker v-model="value1"></date-picker>
+    </div>
+    <div class="demo">
+      <span class="label">range:</span>
+      <date-picker v-model="value2" range></date-picker>
+    </div>
+  </div>
+</template>
+
+<script>
+import DatePicker from './datepicker/index.vue'
+
+export default {
+  name: 'app',
+  components:{ DatePicker },
+  data () {
+    return {
+      value1:'',
+      value2:'',
+    }
+  }
+}
+</script>
+
+<style>
+.demo {
+  float:left;
+  margin:60px;
+}
+.label{
+  margin-right: 1em;
+}
+</style>
