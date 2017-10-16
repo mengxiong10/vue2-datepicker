15f87978f079e545787332216a867e81199d6bf2
Frederico Ferreira <frederico.dida1@gmail.com>
2017-10-15 22:37:44 -0200

    add lang pt-br

--- datepicker/languages.js
+++ datepicker/languages.js
@@ -25,5 +25,14 @@ export default {
       'date': 'Seleccionar fecha',
       'dateRange': 'Seleccionar un rango de fechas'
     }
-  }
+  },
+  'pt-br': {
+    'days': ['Dom','Seg', 'Ter', 'Qua', 'Quin', 'Sex', 'Sáb'],
+    'months': ['Jav', 'Feb', 'Mar', 'Abr', 'Maio', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+    'pickers': ['próximos 7 dias', 'próximos 30 dias', '7 dias anteriores', ' 30 dias anteriores'],
+    'placeholder': {
+      'date': 'Selecione uma data',
+      'dateRange': 'Selecione um período'
+    }
+  },
 }
