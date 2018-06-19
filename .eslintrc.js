3b3cd071d2bf463644357d382cb27bb4c6d3a3f7
mxie <15623530290@163.com>
2018-06-19 14:51:42 +0800

    docs: update docs

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,7 +25,7 @@ module.exports = {
     'generator-star-spacing': 'off',
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-console': process.env.NODE_ENV === 'production' ? ["error", { allow: ["warn", "error"] }] : 'off'
+    'no-console': process.env.NODE_ENV === 'production' ? ["error", { allow: ["warn", "error"] }] : 'off',
     'camelcase': ['off', { properties: 'never' }],
     // "vue/max-attributes-per-line": [2, {
     //   "singleline": 1,
