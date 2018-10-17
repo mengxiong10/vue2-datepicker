a310f59c474aa5b1b8632f30a4cf9093e10edf02
mxie <15623530290@163.com>
2018-10-17 09:58:06 +0800

    feat: when use script type, install automatic

--- src/index.js
+++ src/index.js
@@ -5,4 +5,9 @@ DatePicker.install = function (Vue) {
   Vue.component(DatePicker.name, DatePicker)
 }
 
+/* istanbul ignore if */
+if (typeof window !== 'undefined' && window.Vue) {
+  DatePicker.install(window.Vue)
+}
+
 export default DatePicker
