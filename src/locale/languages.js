fb1abf57d386b0fb9e8831619c4bb9a1f6e325d7
Lee Han Bin <leehan0617@naver.com>
2019-01-03 17:10:42 +0900

    update korea language
    update locale : korean language

--- src/locale/languages.js
+++ src/locale/languages.js
@@ -97,5 +97,14 @@ export default {
       'date': 'Izberite datum',
       'dateRange': 'Izberite razpon med 2 datumoma'
     }
+  },
+  'ko': {
+    'days': ['일', '월', '화', '수', '목', '금', '토'],
+    'months:': ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+    'pickers:': ['다음 7일', '다음 30일', '이전 7일', '이전 30일'],
+    'placeholder': {
+      'date': '날짜를 선택하세요',
+      'dateRange': '날짜 범위를 선택하세요'      
+    }
   }
 }
