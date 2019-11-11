e32299d6a738a626fa3c7cb8524cfc70ca7ee7b0
mengxiong10 <15623530290@163.com>
2019-11-11 18:23:22 +0800

    chore: update babel

--- babel.config.js
+++ babel.config.js
@@ -1,26 +1,29 @@
-module.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        modules: false,
-      },
-    ],
-  ],
-  plugins: ['transform-vue-jsx', '@babel/plugin-transform-object-assign'],
-  env: {
-    test: {
-      presets: [
-        [
-          '@babel/preset-env',
-          {
-            targets: {
-              node: 'current',
-            },
-          },
-        ],
+module.exports = api => {
+  api.cache(false);
+  return {
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          modules: false,
+        },
       ],
-      plugins: ['transform-vue-jsx'],
+    ],
+    plugins: ['transform-vue-jsx', '@babel/plugin-transform-object-assign'],
+    env: {
+      test: {
+        presets: [
+          [
+            '@babel/preset-env',
+            {
+              targets: {
+                node: 'current',
+              },
+            },
+          ],
+        ],
+        plugins: ['transform-vue-jsx'],
+      },
     },
-  },
+  };
 };
