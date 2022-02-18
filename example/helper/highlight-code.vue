d09739f900900624dc72037b02278fb1fb34bbe6
mengxiong10 <15623530290@163.com>
2022-02-18 16:35:00 +0800

    chore: update highlight.js

--- example/helper/highlight-code.vue
+++ example/helper/highlight-code.vue
@@ -6,7 +6,7 @@
 
 <script>
 /* eslint-disable import/no-extraneous-dependencies */
-import hljs from 'highlight.js/lib/highlight';
+import hljs from 'highlight.js/lib/core';
 import javascript from 'highlight.js/lib/languages/javascript';
 import xml from 'highlight.js/lib/languages/xml';
 import 'highlight.js/styles/atom-one-light.css';
